4c11aed1f3bf9bdb54f75e4732506a9eafe5d00e
davidv410 <davidvrgoc99@gmail.com>
2026-05-25 19:24:21 +0200

    resend added

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-	  limit: 5,
+	  limit: 1,
     standardHeaders: true,
     legacyHeaders: false, 
     ipv6Subnet: 56,
--- utils/emailService.js
+++ utils/emailService.js
@@ -1,72 +1,53 @@
-const nodemailer = require('nodemailer')
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-    }
-})
 
 const sendContactEmail = async (name, email, phone, message) => {
-    try{
-        const mailStructure = { 
-            from: email,
-            to: process.env.EMAIL, 
-            subject: `KONTAKT`,
-            text: `${name} (${email} - ${phone})
-            ${message}` 
-        }
-        return transporter.sendMail(mailStructure)
-    }catch(err){
-        console.error("SMTP ERROR:", err);
-        throw err;
-    }
-}
+  return await resend.emails.send({
+    from: "Konoba Ivina Arka <onboarding@resend.dev>",
+    to: process.env.EMAIL,
+    subject: "KONTAKT",
+    text: `${name} (${email} - ${phone})\n\n${message}`,
+  });
+};
 
 const sendBookingConfirmation = async (name, email, phone, date, time, people) => {
     const options = { year: 'numeric', month: 'short', day: 'numeric' };
     let dateFormat = new Date(date).toLocaleDateString('en-US', options);
 
     const [hours, minutes] = time.split(":");
-    let timeFormat = `${hours}:${minutes}`
+    let timeFormat = `${hours}:${minutes}`;
 
-    const mailStructure = {
-            from: 'KONOBA IVINA ARKA',
-            to: email,
-            subject: 'REZERVACIJA',
-            text: `Poštovani ${name}. Vaša rezervacija ${dateFormat} u ${timeFormat} za ${people}, je uspješna. Vidimo se!`
-    }
-
-    return transporter.sendMail(mailStructure)
-}
+    return await resend.emails.send({
+        from: "Konoba Ivina Arka <onboarding@resend.dev>",
+        to: email,
+        subject: "REZERVACIJA",
+        text: `Poštovani ${name}. Vaša rezervacija ${dateFormat} u ${timeFormat} za ${people} je uspješna. Vidimo se!`
+    });
+};
 
 const sendBookingRejection = async (name, email, phone, date, time, people) => {
     const options = { year: 'numeric', month: 'short', day: 'numeric' };
     let dateFormat = new Date(date).toLocaleDateString('en-US', options);
 
     const [hours, minutes] = time.split(":");
-    let timeFormat = `${hours}:${minutes}`
+    let timeFormat = `${hours}:${minutes}`;
 
-    const mailStructure = {
-            from: 'KONOBA IVINA ARKA',
-            to: email,
-            subject: 'REZERVACIJA',
-            text: `Poštovani ${name}. Vaša rezervacija ${dateFormat} u ${timeFormat}, nažalost nije uspješna.`
-        }
-    return transporter.sendMail(mailStructure)
-}
+    return await resend.emails.send({
+        from: "Konoba Ivina Arka <onboarding@resend.dev>",
+        to: email,
+        subject: "REZERVACIJA",
+        text: `Poštovani ${name}. Vaša rezervacija ${dateFormat} u ${timeFormat}, nažalost nije uspješna.`
+    });
+};
 
 const bookingConfirmationNotification = async (email, konoba) => {
-    let mailStructure = {
-        from: email,
+    return await resend.emails.send({
+        from: "Konoba Ivina Arka <onboarding@resend.dev>",
         to: konoba,
-        subject: 'ZAHTJEV ZA REZERVACIJU',
-        text: `Zahtjev za rezervaciju od ${email}, potvrdi/odbij u admin panelu, https://www.konobaivinaarka.com/admin-confirm` 
-    }
-    return transporter.sendMail(mailStructure)
-}
+        subject: "ZAHTJEV ZA REZERVACIJU",
+        text: `Zahtjev za rezervaciju od ${email}, potvrdi/odbij u admin panelu: https://www.konobaivinaarka.com/admin-confirm`
+    });
+};
 
 module.exports = { sendContactEmail, sendBookingConfirmation, sendBookingRejection, bookingConfirmationNotification }
