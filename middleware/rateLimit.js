2ed26f266e85eb4695d8ff897f82b633c9b70648
davidv410 <davidvrgoc99@gmail.com>
2026-05-25 19:27:23 +0200

    resend added #2

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-	  limit: 1,
+	  limit: 5,
     standardHeaders: true,
     legacyHeaders: false, 
     ipv6Subnet: 56,
