349106bc00383b0a90a7635e880dbaca87a4a775
davidv410 <davidvrgoc99@gmail.com>
2026-05-25 18:58:47 +0200

    nodemailer fix

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {rejectUnauthorized: false}
+    }
 })
 
 const sendContactEmail = async (name, email, phone, message) => {
